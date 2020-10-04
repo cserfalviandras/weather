4b3fdedb27e7b17a32049ab2c9d25c965ec81a4d
Cserfalvi_Andras <cserfalviandras@gmail.com>
2020-10-04 10:15:05 +0200

    Add route for fetching detailed forecast.

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,5 @@ use App\Http\Controllers\WeatherController;
 Route::get('/weather/{city}', [WeatherController::class, 'show']);
 
 Route::get('/weather/{city}/getWeather', [WeatherController::class, 'getCityWeatherData']);
+
+Route::get('/weather/{city}/detailedForecast', [WeatherController::class, 'getDetailedForecast']);
